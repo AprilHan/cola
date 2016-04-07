9424298749bb8ed4e7331842c7edfe0670c6f010
AprilHan <1035839894@qq.com>
2016-04-07 22:55:43 +0800

    完成预加载 点击事件

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 /*
 * @Author: mm
 * @Date:   2016-03-24 23:14:24
-* @Last Modified by:   mm
-* @Last Modified time: 2016-03-24 23:14:40
+* @Last Modified by:   AprilHan
+* @Last Modified time: 2016-04-07 21:10:52
 */
 
+(function($){
+  var pullDown = document.querySelector(".pullDown");
+
+  pullDown.addEventListener("click",function(){
+      $.fn.fullpage.moveSectionDown();
+  })
+})($);
 
 
